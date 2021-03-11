6267de7dd7e92fe8fcecef1adbbf6924052e1079
agmarchuk <mag@iis.nsk.su>
2021-03-11 16:04:29 +0700

    Сформирован новый проект Flinkinterpret

---
+++ FlinkInterpret/MySource.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FlinkInterpret
+{
+    public class MySource : IEnumerator
+    {
+        private string[] words;
+        private int nwordsinline, nlines;
+        Random rnd, rnd2;
+        public MySource(double pspace, int primarytext_len, int nwordsinline, int nlines)
+        {
+            this.nwordsinline = nwordsinline;
+            this.nlines = nlines;
+            const string letters = "abcdefghijklmnopqrstuwyxyz";
+            char[] chars = new char[primarytext_len];
+            rnd = new Random(777777);
+            for (int i = 0; i<primarytext_len; i++)
+            {
+                if (rnd.NextDouble() <= pspace) chars[i] = ' ';
+                else chars[i] = letters[rnd.Next(letters.Length)];
+            }
+            string primarytext = new string(chars);
+            words = primarytext.Split(' ', StringSplitOptions.RemoveEmptyEntries).Distinct().ToArray();
+            rnd2 = new Random(1234567);
+        }
+        private string line = null;
+        private int iline = 0;
+        public object Current => line;
+
+        public bool MoveNext()
+        {
+            if (iline >= nlines) return false;
+            iline++;
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < nwordsinline; i++)
+            {
+                if (i != 0) sb.Append(' ');
+                sb.Append(words[rnd2.Next(words.Length)]);
+            }
+            line = sb.ToString();
+            return true;
+        }
+
+        public void Reset()
+        {
+            iline = 0;
+            rnd2 = new Random(1234567);
+        }
+
+        object IEnumerator.Current
+        {
+            get { return Current; }
+        }
+
+        // Для сравнения эффективности
+        public IEnumerable<object> Elements()
+        {
+            for (int j = 0; j < nlines; j++)
+            {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < nwordsinline; i++)
+                {
+                    if (i != 0) sb.Append(' ');
+                    sb.Append(words[rnd2.Next(words.Length)]);
+                }
+                yield return sb.ToString();
+            }
+        }
+    }
+}
---
+++ FlinkInterpret/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FlinkInterpret
+{
+
+    class Program
+    {
+        public static System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start Flink Interpret");
+            sw.Restart();
+            var source = new MySource(0.2, 10000, 50, 200_000);
+            sw.Stop(); Console.WriteLine("=== duration=" + sw.ElapsedMilliseconds);
+
+            sw.Restart();
+            long cnt_symbls = 0;
+            string line = "start";
+            while (source.MoveNext())
+            {
+                //Console.WriteLine(source.Current);
+                cnt_symbls += ((string)source.Current).Length;
+                line = (string)source.Current;
+            }
+            Console.WriteLine(cnt_symbls + " symbols");
+            Console.WriteLine(line);
+            sw.Stop(); Console.WriteLine("=== duration=" + sw.ElapsedMilliseconds);
+
+            sw.Restart();
+            source.Reset();
+            long cnt = 0;
+            foreach (string v in source.Elements())
+            {
+                cnt += v.Length;
+                line = v;
+            }
+            Console.WriteLine(cnt + " symbols");
+            Console.WriteLine(line);
+            sw.Stop(); Console.WriteLine("=== duration=" + sw.ElapsedMilliseconds);
+
+            // =============== Попытка посчитать количество слов =================
+            sw.Restart();
+            source.Reset();
+            cnt = 0;
+            Dictionary<string, int> hash_dictionary = new Dictionary<string, int>();
+            while (source.MoveNext())
+            {
+                line = (string)source.Current;
+                string[] words = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                foreach (string word in words)
+                {
+                    cnt++;
+                    string key = word;
+                    int val = 1;
+                    if (!hash_dictionary.ContainsKey(key))
+                    {
+                        hash_dictionary.Add(key, 1);
+                    }
+                    else
+                    {
+                        hash_dictionary[key] += 1;
+                    }
+                }
+            }
+            Console.WriteLine($"total words: {cnt}");
+            sw.Stop(); Console.WriteLine("=== duration=" + sw.ElapsedMilliseconds);
+            foreach (var pair in hash_dictionary)
+            {
+                Console.WriteLine($"{pair.Key} {pair.Value}");
+            }
+
+        }
+    }
+}
